f976a39c35aaa2fe5dac299188e43d542490fa69
Eder Domenici <domenicieder@gmail.com>
2022-07-05 22:13:34 -0300

    Subindo backend

--- src/routes/ProductsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const ProductsController = require('../controllers/ProductsController')
-const router = express.Router();
-
-
-router.get('/', ProductsController.index)
-
-module.exports = router;
