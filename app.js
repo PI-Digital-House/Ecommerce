bf82546924513958e0b44d7d27f7cd23ea354a5c
Eder Domenici <domenicieder@gmail.com>
2022-07-08 20:27:50 -0300

    sequel implementado

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const ProductsController = require('./src/controllers/ProductsController');
 const ProductRoutes = require('./src/routes/ProductsRoutes');
+//fdsakçljfçklsadjfçlksadhnfçjklsadbh
+
+fdçlfdmkfhasldkjfsadkçljfçlksadjfçklsadjflkçjsadhflkjsdhafkjhsadkljfhsdakljhfhsdfjkhfkljsadhfjkhc
+const a = 1234;
