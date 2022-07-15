c83b8bc6be4eec5dc2719df642c21deac855d704
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2022-07-15 20:17:01 -0300

    Fusão de Branchs
    Fusão de Branchs
    
    Backend3 + Jean EJS, para remover os conflitos da ultima pull resquest

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const db = require('./models/index')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,9 +18,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-//Banco de Dados
-
 
 app.use(logger('dev'));
 app.use(express.json());
