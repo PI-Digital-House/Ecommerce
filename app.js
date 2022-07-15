44200ba54cb13047548124abdae58a104ee9edf5
Eder Domenici <domenicieder@gmail.com>
2022-07-15 12:58:40 -0300

    Express e sequelize funcional

--- app.js
+++ app.js
@@ -3,25 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const db = require('./models/index')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var homeRouter = require('./routes/home');
-var profileRouter = require('./routes/profile');
-var cartRouter = require('./routes/cart');
-var productsRouter = require('./routes/products')
-var signInRouter = require('./routes/signin')
-var signUpRouter = require('./routes/signup')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-//Banco de Dados
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,12 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/home', homeRouter);
-app.use('/profile', profileRouter);
-app.use('/cart', cartRouter);
-app.use('/products', productsRouter);
-app.use('/signin', signInRouter);
-app.use('/signup', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
