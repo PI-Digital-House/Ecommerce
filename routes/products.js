1df26e10f0a613386f94f529e63c280613a4e1f3
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2022-08-01 19:02:52 -0300

    Subindo  Pagina de Cadastro de Produtos
    Layout EJS -> Funcional
    Estilo CSS -> Funcional
    Armazenamento Localstorage com JS -> Funcional

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Pagina Products Funcionando');
+  res.render('products.ejs');
 });
 
 module.exports = router;
