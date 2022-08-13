a72048f0dec3e0f585690caed12a547215cdddec
Eder Domenici <domenicieder@gmail.com>
2022-08-13 14:50:39 -0300

    Integrando EJS

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 
 
 const customerRouter = require('./routes/customer');
-const usersRouter = require('./routes/users');
+const homeRouter = require('./routes/home')
 
 
 const app = express();
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/customer', customerRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
