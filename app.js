4c2e2007751bc4c73db59ed9e1ed5f9b2ee6c3b8
demoner21 <a.tranhago@gmail.com>
2022-08-23 21:29:07 -0300

    no changes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const app = express();
 //render public paste
 app.use('/views', express.static(path.resolve(__dirname, 'views')));
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
+app.use('/views', express.static(path.resolve(__dirname, 'views')));
 app.use('/javascripts', express.static(path.resolve(__dirname, 'javascripts')));
 
 // view engine setup
